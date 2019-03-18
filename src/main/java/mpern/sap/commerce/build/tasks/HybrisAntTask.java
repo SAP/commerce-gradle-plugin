b50734092d3527fb4cc03062d16f8988258ae85e
Markus Perndorfer <m.perndorfer@gmx.at>
2019-03-18 08:43:40 +0100

    fix 10: do not forward system properties

--- src/main/java/mpern/sap/commerce/build/tasks/HybrisAntTask.java
+++ src/main/java/mpern/sap/commerce/build/tasks/HybrisAntTask.java
@@ -27,22 +27,17 @@ public class HybrisAntTask extends JavaExec {
             if (task instanceof HybrisAntTask) {
                 HybrisAntTask t = (HybrisAntTask) task;
 
-                Map<String, Object> systemProperties = fetchSystemProperties();
-                t.systemProperties(systemProperties);
-
-                t.antProperty("maven.update.dbdrivers", "false");
-
                 ConfigurableFileTree files = buildPlatformAntClasspath(t);
                 t.setClasspath(files);
+                t.setMain("org.apache.tools.ant.launch.Launcher");
 
                 HybrisPlatform platform = ((HybrisPluginExtension) t.getProject().getExtensions().getByName(HybrisPlugin.HYBRIS_EXTENSION)).getPlatform();
                 t.systemProperty("ant.home", platform.getAntHome().get().getAsFile());
 
-                t.setMain("org.apache.tools.ant.launch.Launcher");
+                t.antProperty("maven.update.dbdrivers", "false");
+                t.antProperties.forEach((k, v) -> t.args("-D" + k + "=" + v));
 
                 t.workingDir(platform.getPlatformHome());
-
-                t.antProperties.forEach((k, v) -> t.args("-D" + k + "=" + v));
             }
         }
 
@@ -50,14 +45,6 @@ public class HybrisAntTask extends JavaExec {
             ConfigurableFileTree files = t.getProject().fileTree("hybris/bin/platform");
             files.include("apache-ant*/lib/ant-launcher.jar");
             return files;
-        }
-
-        private Map<String, Object> fetchSystemProperties() {
-            Properties properties = System.getProperties();
-            Map<String, Object> propertyMap = new HashMap<>();
-            properties.forEach((key, value) -> propertyMap.put(String.valueOf(key), value));
-            propertyMap.remove("user.dir");
-            return propertyMap;
         }
     }
 
